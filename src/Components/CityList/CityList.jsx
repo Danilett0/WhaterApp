c23a0d86bbb9370d5c8bc4ecb235379980b814e8
jeferson.castro <jeferson.castro@repremundo.com.co>
2022-10-27 18:29:23 -0500

    agrego alerta para control de errores

--- src/Components/CityList/CityList.jsx
+++ src/Components/CityList/CityList.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import CityInfo from "./../CityInfo";
-import Alert from '@material-ui/lab/Alert'
-import convertUnits from "convert-units"
+import Alert from "@material-ui/lab/Alert";
+import convertUnits from "convert-units";
 import Weather from "./../Weather";
 import "./CityList.css";
 
@@ -14,68 +14,83 @@ const renderCityAndCountry =
 
     return (
       <li className="CitiesList" key={city} onClick={eventonClickCity}>
-
         <CityInfo city={city} country={country} />
 
-        <Weather temperature={weather && weather.temperature} state={weather && weather.state} />
-        
+        <Weather
+          temperature={weather && weather.temperature}
+          state={weather && weather.state}
+        />
       </li>
     );
   };
 
 const CityList = ({ cities, onClickCity }) => {
+  const [AllWeather, setAllWeather] = useState({});
+  const [error, setError] = useState(null);
 
- const [AllWeather, setAllWeather] =  useState({})
+  // realizo el consumo
+  useEffect(() => {
+    const setWeather = (city, country, countryCode) => {
+      const appid = "1988644c6e1d994a6dca62d33fe09348";
 
-// realizo el consumo 
- useEffect(() => {
-  
-  const setWeather = (city, country, countryCode) => {
-    const appid = "1988644c6e1d994a6dca62d33fe09348";
-    
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${appid}`;
-    
-    axios.get(url).then(response => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${appid}`;
 
-      const {data} = response;
-      const temperature = Number(convertUnits(data.main.temp).from("K").to("C").toFixed(0))
-      const state = data.weather[0].main.toLowerCase();
+      axios
+        .get(url)
+        .then((response) => {
+          const { data } = response;
+          const temperature = Number(
+            convertUnits(data.main.temp).from("K").to("C").toFixed(0)
+          );
+          const state = data.weather[0].main.toLowerCase();
 
-      const propName = `${city} - ${country}`; // ['Buenos aires - argentina']
-      const propValue = {temperature, state}; // {temperature: 10 , state: 'sunny' }
+          const propName = `${city} - ${country}`; // ['Buenos aires - argentina']
+          const propValue = { temperature, state }; // {temperature: 10 , state: 'sunny' }
 
-      setAllWeather( (AllWeather) => ( { ...AllWeather, [propName]: propValue } ) );
-       
-    }).catch(error =>  {
+          setAllWeather((AllWeather) => ({
+            ...AllWeather,
+            [propName]: propValue,
+          }));
+        })
+        .catch((error) => {
+          if (error.response) {
+            const { data, status } = error.response;
+            console.log(
+              "mi servidor me respondio un error:",
+              data.message,
+              status
+            );
+            setError("Ha ocurrido un error en el servidor del clima");
+          } else if (error.request) {
+            console.log(
+              "servidor no disponible o no hay conexxio de internet "
+            );
+            setError("Por favor verifique su conexxio de internet");
+          } else {
+            console.log("errores imprevistos");
+            setError("Error al cargar los datos del clima");
+          }
+        });
+    };
 
-      if (error.response){
-        const {data, status} = error.response;
-        console.log("mi servidor me respondio un error:",data.message, status);
-      }else if(error.request){
-        console.log("servidor no disponible o no hay conexxio de internet ")
-      }else{
-        console.log("errores imprevistos");
-      }
-
-
-    })
-  }
-
-  // realizo destructuring de las propiedades del objeto
-  cities.forEach(({city, country, countryCode }) => {
-    setWeather(city,country, countryCode)
-  });
-
- }, [cities]) // array de dependencias
- 
+    // realizo destructuring de las propiedades del objeto
+    cities.forEach(({ city, country, countryCode }) => {
+      setWeather(city, country, countryCode);
+    });
+  }, [cities]); // array de dependencias
 
   return (
-    <ul className="citys">
-      {cities.map((cityAndCountry) => renderCityAndCountry(onClickCity)(cityAndCountry, AllWeather[`${cityAndCountry.city} - ${cityAndCountry.country}`]) )}
-    </ul>
+    <div>
+      
+      {error && <Alert severity="error"> {error} </Alert> }
+
+      <ul className="citys"> {cities.map((cityAndCountry) => 
+        renderCityAndCountry(onClickCity)( 
+        cityAndCountry, AllWeather[`${cityAndCountry.city} - ${cityAndCountry.country}`] ) )}
+      </ul>
+
+    </div>
   );
-
-
 };
 
 CityList.propTypes = {
@@ -83,7 +98,7 @@ CityList.propTypes = {
     PropTypes.shape({
       city: PropTypes.string.isRequired,
       country: PropTypes.string.isRequired,
-      countryCode: PropTypes.string.isRequired
+      countryCode: PropTypes.string.isRequired,
     })
   ).isRequired,
   onClickCity: PropTypes.func.isRequired,
