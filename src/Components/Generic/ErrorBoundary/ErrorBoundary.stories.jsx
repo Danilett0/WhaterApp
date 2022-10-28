13d6d6d856c1b23cbb7272a533a86ad0536bb2ae
jeferson.castro <jeferson.castro@repremundo.com.co>
2022-10-27 21:27:17 -0500

    agrego clase a react

---
+++ src/Components/Generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ErrorBoundery from './ErrorBoundery';
+
+
+export default {
+    title: 'ErrorBoundery',
+    Comment: ErrorBoundery
+}
+
+
+export const ErrorBounderyExample = () => <ErrorBoundery saludo="Bienvenido Panita" />
---
+++ src/Components/Generic/ErrorBoundary/ErrorBoundery.jsx
@@ -0,0 +1,14 @@
+import React , {Component} from 'react'
+
+
+class ErrorBoundery extends Component {
+
+
+
+    render(){
+        return <h1> {this.props.saludo} </h1>
+    }
+
+}
+
+export default ErrorBoundery
