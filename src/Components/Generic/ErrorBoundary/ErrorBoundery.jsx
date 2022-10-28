3cf24d84330db020d05532cd88eaa221b982af8d
jeferson.castro <jeferson.castro@repremundo.com.co>
2022-10-27 21:42:41 -0500

    agrego estado a mi clase

--- src/Components/Generic/ErrorBoundary/ErrorBoundery.jsx
+++ src/Components/Generic/ErrorBoundary/ErrorBoundery.jsx
@@ -1,14 +1,27 @@
-import React , {Component} from 'react'
-
+import React, { Component } from "react";
 
 class ErrorBoundery extends Component {
 
+  constructor(props){
+    super(props)
 
-
-    render(){
-        return <h1> {this.props.saludo} </h1>
+    this.state = {
+        activo: false,
     }
+  }
 
+  estaACtivo = () => {
+    return this.state.activo ? "activo" : "no esta activo";
+  };
+
+  render() {
+    return (
+      <>
+      <h1>{this.props.saludo}</h1>
+      <h2>{this.estaACtivo()}</h2>
+      </>
+    );
+  }
 }
 
-export default ErrorBoundery
+export default ErrorBoundery;
