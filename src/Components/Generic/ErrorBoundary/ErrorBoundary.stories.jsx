1441266f68955311764a53debe5cc32d583f9e91
jeferson.castro <jeferson.castro@repremundo.com.co>
2022-10-27 22:29:39 -0500

    estados por los que pasa una clase

--- src/Components/Generic/ErrorBoundary/ErrorBoundary.stories.jsx
+++ src/Components/Generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import ErrorBoundery from './ErrorBoundery';
-
+import React from "react";
+import ErrorBoundery from "./ErrorBoundery";
 
 export default {
-    title: 'ErrorBoundery',
-    Comment: ErrorBoundery
-}
+  title: "ErrorBoundery",
+  Comment: ErrorBoundery,
+};
 
+CommentWithoutError = () => <h1> sin error</h1>;
 
-export const ErrorBounderyExample = () => <ErrorBoundery saludo="Bienvenido Panita" />
+const prop = undefined;
+CommentWithError = () => <h1>{ prop.hola }</h1>;
+
+export const ErrorBounderyWithError = () => (
+  <ErrorBoundery>
+    <CommentWithError />
+  </ErrorBoundery>
+);
+
+export const ErrorBounderyWithoutError = () => (
+  <ErrorBoundery>
+    <CommentWithoutError />
+  </ErrorBoundery>
+);
--- src/Components/Generic/ErrorBoundary/ErrorBoundery.jsx
+++ src/Components/Generic/ErrorBoundary/ErrorBoundery.jsx
@@ -14,11 +14,30 @@ class ErrorBoundery extends Component {
     return this.state.activo ? "activo" : "no esta activo";
   };
 
+  onClickCityHandler = () => {
+    this.setState({activo: true});
+  }
+
+  componentDidMount(){
+    console.log("el componente se ha montado");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Estado previo" , prevState.activo);
+    console.log("Estado nuevo" , this.state.activo);
+    console.log("el componente se ha actualizado");
+  }
+
+  componentWillUnmount(){
+    console.log("el componente se ha montado");
+  }
+
   render() {
     return (
       <>
       <h1>{this.props.saludo}</h1>
       <h2>{this.estaACtivo()}</h2>
+      <button onClick={ () => {this.onClickCityHandler()} }> Activar</button>
       </>
     );
   }
